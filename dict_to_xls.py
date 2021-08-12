ae057d8e3d81c55e392c99e9ce401811d28fd798
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2021-08-12 13:23:50 +0500

    dict_to_xls_conv

---
+++ dict_to_xls.py
@@ -0,0 +1,48 @@
+from utilites import load, dump
+
+
+def dict_to_xls(filename = 'out.xlsx', 
+                 IN = {}, 
+                 structure = {
+                         "sheets":1,
+                         "columns":2,
+                         "rows":0}):
+    def shorter(name):
+        try:
+            assert len(name) <= 31, "Excel limitation!"
+            short_name = name
+        except AssertionError:
+            short_name = name.split()[0]
+        return {name:short_name}
+    
+    import xlwt
+    wb = xlwt.Workbook(encoding = 'UTF-8')
+    levels = []
+    levels += [sorted(list(IN.keys()))]
+    levels += [sorted(list(IN[levels[0][0]].keys()))]
+    levels += [sorted(list(IN[levels[0][0]][levels[1][0]].keys()))]
+    print(levels[1])
+    sheetnames = {}
+    for key0 in levels[structure['sheets']]:
+        sheetnames.update(shorter(key0))
+        ws = wb.add_sheet(sheetnames[key0])
+        i = 1
+        for key1 in levels[structure['columns']]:
+            ws.write(0, i, key1)
+            j = 1
+            for key2 in levels[structure['rows']]:
+                print(key2, key0, key1)
+                ws.write(j, i, IN[key0][key1][key2])
+                j += 1
+            i += 1
+        i = 1
+        for key2 in levels[structure['rows']]:
+            ws.write(i, 0, key2)
+            i += 1
+    
+    
+    wb.save(filename)
+    
+data = load('data/normalized_table.json')
+
+dict_to_xls('/tmp/out.xlsx', data, {'sheets':0,'columns':1, 'rows':2})
